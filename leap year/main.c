3fb80e9c20e9c0cd5d4ca821732d261c15fb1f6f
sanaa <Sanaakumar07@gmail.com>
2025-09-13 12:07:50 +0530

    check if year is leap year or not

--- leap year/main.c
+++ leap year/main.c
@@ -1,14 +1,12 @@
-//
-//  main.c
-//  leap year
-//
-//  Created by Sanaa Kumar on 13/09/25.
-//
-
 #include <stdio.h>
-
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    int y;
+    printf("enter year = ");
+    scanf("%d",&y);
+    if ((y%4==0 && y%100 !=0) || y%400==0){
+        printf("year is a leap year");
+    }
+    else
+        printf("year is not a leap year");
     return 0;
 }
